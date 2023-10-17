becd907a4096146772ff69867ec1489bf6ea3ef7
Kahoneki <shinyzorua6@gmail.com>
2023-10-17 18:07:29 +0100

    Linearising the depth buffer

--- DepthTest/Source Files/Shaders/Object/frag.glsl
+++ DepthTest/Source Files/Shaders/Object/frag.glsl
@@ -5,7 +5,18 @@ in vec2 TexCoords;
 
 uniform sampler2D texture1;
 
+float near = 0.1;
+float far = 100.0;
+
+float LineariseDepth(float depth)
+{
+    float ndc = depth * 2.0 - 1.0;
+    float linearDepth = (2.0 * near * far) / (far + near - ndc * (far - near));
+    return linearDepth;
+}
+
 void main()
 {    
-    FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+    float depth = LineariseDepth(gl_FragCoord.z) / far; //Divide by far for demonstration
+    FragColor = vec4(vec3(depth), 1.0);
 }
